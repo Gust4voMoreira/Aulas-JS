adf7ce314a8920d095544912bdcbe428b54f881d
Gustavo Moreira <gm1230133@gmail.com>
2021-03-10 14:27:27 -0400

    aulas e testes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aulas_e_Testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
