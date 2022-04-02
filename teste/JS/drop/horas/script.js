b57cc4fea27c086fe3bab6496f39f10f4e24cd49
Gustavo Moreira <gust4vo.vasconcelos@gmail.com>
2022-04-02 10:51:57 -0400

    alteração

--- teste/JS/drop/horas/script.js
+++ teste/JS/drop/horas/script.js
@@ -7,13 +7,13 @@ function carregar() {
     msg.innerHTML = (`Agora são ${hora} horas.`)
 
     if(hora >= 0 && hora < 12) {
-        img.src = '/aulas/Javascript/aula12ex/ex014/fotomanha.png'
+        img.src = '/teste/JS/drop/horas/fotomanha.png'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12  && hora <= 18) {
-        img.src = '/aulas/Javascript/aula12ex/ex014/fototarde.png'
+        img.src = '/teste/JS/drop/horas/fototarde.png'
         document.body.style.background = '#b9846f'
     } else {
-        img.src = '/aulas/Javascript/aula12ex/ex014/fotonoite.png'
+        img.src = '/teste/JS/drop/horas/fotonoite.png'
         document.body.style.background = '#515154'
     }
 }
